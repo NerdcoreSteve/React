4b76e0dc9885f7dae218797ea7c2e368c66bc107
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-01-13 14:07:34 -0700

    got react working

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -3,6 +3,26 @@ require('whatwg-fetch')
 const
     R = require('ramda'),
     Rx = require('rx'),
-    tap = x => { console.log(x); return x }
+    tap = x => { console.log(x); return x },
+    React = require('react'),
+    ReactDOM = require('react-dom'),
+    SomeComponent = () =>
+        <div>This is a stateless functional component</div>,
+    AnotherComponent = ({argument}) =>
+        <div>`The argument of this component is {argument}`</div>
 
-console.log('whattup?')
+ReactDOM.render(
+        <p>Yo yo whattup?</p>,
+    document.getElementById('hello'))
+
+ReactDOM.render(
+    <div>
+        <SomeComponent/>
+        <AnotherComponent argument='bananas'/>
+    </div>,
+    document.getElementById('simple'))
+
+/*
+TODO
+With redux, make a text box that changes an element when you type in it
+*/
